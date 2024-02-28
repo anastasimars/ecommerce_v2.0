93add70498513ef05b24f06ad5f9871f985d1ae7
anastasimars <sutckina.n@gmail.com>
2024-02-28 13:10:47 +0100

    init commit

---
+++ ecommerce/src/test/java/pl/akademiaspecjalistowit/ecommerce/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.akademiaspecjalistowit.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
