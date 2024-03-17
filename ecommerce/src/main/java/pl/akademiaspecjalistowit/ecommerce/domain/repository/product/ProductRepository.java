a972c13905ed7bc575727fcc30b840bcef46c00b
anastasimars <sutckina.n@gmail.com>
2024-03-17 11:50:48 +0100

    added DataProductService

---
+++ ecommerce/src/main/java/pl/akademiaspecjalistowit/ecommerce/domain/repository/product/ProductRepository.java
@@ -0,0 +1,15 @@
+package pl.akademiaspecjalistowit.ecommerce.domain.repository.product;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import pl.akademiaspecjalistowit.ecommerce.domain.model.ProductEntity;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
+    @Query("SELECT i FROM ItemEntity i WHERE i.technicalId=:technicalId")
+    Optional<ProductEntity> findByTechnicalId(UUID technicalId);
+}
