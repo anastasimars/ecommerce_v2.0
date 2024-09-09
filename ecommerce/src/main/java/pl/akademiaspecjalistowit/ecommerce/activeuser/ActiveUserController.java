edb1e76851b263f5183a6bbb1a190196df007b55
Anastasiia Siutkina <120054681+anastasimars@users.noreply.github.com>
2024-09-09 21:19:31 +0200

    split the logic in ProductController (#17)

--- ecommerce/src/main/java/pl/akademiaspecjalistowit/ecommerce/activeuser/ActiveUserController.java
+++ ecommerce/src/main/java/pl/akademiaspecjalistowit/ecommerce/activeuser/ActiveUserController.java
@@ -1,12 +1,16 @@
 package pl.akademiaspecjalistowit.ecommerce.activeuser;
 
+import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
 import pl.akademiaspecjalistowit.api.UsersApi;
 import pl.akademiaspecjalistowit.model.LoginRequest;
 import pl.akademiaspecjalistowit.model.LoginResponse;
 import pl.akademiaspecjalistowit.model.RegistrationRequest;
 import pl.akademiaspecjalistowit.model.RegistrationResponse;
 
+@RestController
+@AllArgsConstructor
 class ActiveUserController implements UsersApi {
     @Override
     public ResponseEntity<LoginResponse> loginUser(LoginRequest loginRequest) {
